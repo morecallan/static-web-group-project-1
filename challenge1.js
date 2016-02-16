a596f40995468bda1c2e79a9242556eabc3db462
Callan Morrison <callan.l.morrison@gmail.com>
2016-02-15 19:52:07 -0600

    added references to objects

---
+++ challenge1.js
@@ -0,0 +1,10 @@
+var inputText = document.getElementById("inputText");
+var addButton = document.getElementById("addButton");
+
+var cardholderDiv = document.getElementById("cardHolder");
+
+var fontColor = document.getElementById("fontColor");
+var backgroundColor = document.getElementById("backgroundColor");
+
+var deleteButton = document.getElementById("deleteButton");
+
