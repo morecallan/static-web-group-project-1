ee2b21057871d813fd6f9567100ce0512118c583
Callan Morrison <callan.l.morrison@gmail.com>
2016-02-15 20:06:25 -0600

    got cards to add

--- challenge1.js
+++ challenge1.js
@@ -1,4 +1,3 @@
-var inputText = document.getElementById("inputText");
 var addButton = document.getElementById("addButton");
 
 var cardholderDiv = document.getElementById("cardHolder");
@@ -8,3 +7,18 @@ var backgroundColor = document.getElementById("backgroundColor");
 
 var deleteButton = document.getElementById("deleteButton");
 
+addButton.addEventListener("click", addCard);
+
+function addCard() {
+    var inputText = document.getElementById("inputText").value;
+    var cardInput = "<article class='card'>";
+    cardInput += "<input type='color' name='fontColor' value='#ff0000'>";
+    cardInput += "<label for='fontColor'>Font Color</label>";
+    cardInput += "<input type='color' name='backgroundColor' value='#ff0000'>";
+    cardInput += "<label for='backgroundColor'>Background Color</label>";
+    cardInput += "<button type='' class='deleteButton'>Delete</button>";
+    cardInput += "<p>" + inputText + "</p>";
+    cardInput += "</article>";
+
+    cardholderDiv.innerHTML = cardInput + cardholderDiv.innerHTML;
+}
